b85056fe8dc31e8ae1f976b23ee82f0b10a9304b
Andressa Marçal <andressagb@gmail.com>
2019-12-15 22:19:27 -0300

    Adiciona biblioteca urllib3 e altera nome da app

--- django_vote_image/urls.py
+++ django_vote_image/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from app.views import ImageList
+from vote.views import ImageList
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
